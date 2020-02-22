e6a632a63bde6ebcb9d569c4785093d99c4675fe
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-02-22 18:46:04 +0100

    Algorithms and data structures. Classwork. Added homework.

---
+++ src/Algorytmy/rekurencja/Hanoi.java
@@ -0,0 +1,33 @@
+package Algorytmy.rekurencja;
+
+import java.util.Scanner;
+
+public class Hanoi {
+    /*
+    9.	Napisz program realizujący wieże Hanoi
+    1, n = 1
+    2 * hanoi(n-1), n >1
+
+     */
+
+    public static void main(String[] args) {
+        Scanner scannner = new Scanner(System.in);
+        int n;
+        System.out.println("Podaj n");
+        n = scannner.nextInt();
+        System.out.println("Dla n = " + n + " Hanoi = " + hanoi(n));
+        for (int i = 1; i <= n ; i++) {
+            System.out.println("Dla n = " + i + " Hanoi = " + hanoi(i));
+
+
+        }
+    }
+
+    public static int hanoi(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return 2 * hanoi(n - 1);
+        }
+    }
+}
