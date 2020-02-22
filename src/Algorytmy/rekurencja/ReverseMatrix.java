58900da8b47042da12bfb0a2bc7c82f0ecfa5c28
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-02-22 17:12:36 +0100

    Algorithms and data structures. Classwork. Added homework.

---
+++ src/Algorytmy/rekurencja/ReverseMatrix.java
@@ -0,0 +1,27 @@
+package Algorytmy.rekurencja;
+
+import java.util.Arrays;
+
+public class ReverseMatrix {
+    static public void main(String[] args) {
+        int [] tablica = {1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(tablica));
+        reverseMatrix(tablica, 0); // odwracamy element nr 0 i ostatni, wywolujac kolejno 1 i n-1,2 n-2
+        System.out.println(Arrays.toString(tablica));
+    }
+
+    /*
+       1.	Załóżmy, że chcemy odwrócić w sposób rekurencyjny tablicę liczb całkowitych.
+       Zaproponuj algorytm z użyciem rekurencji, który wykona to zadanie
+        */
+    static void reverseMatrix(int [] tablica, int i) {
+       // tablica wejsciowa tab oraz numer elementu dla ktorego inwerja
+        // zamieniamy parami, i-ty z (length-1)-i
+        int j = tablica.length - 1 - i; //
+        if (i >= j) return;
+        int pom = tablica[i];
+        tablica[i] = tablica[j];
+        tablica[j] = pom;
+        reverseMatrix(tablica, i + 1); // wywolujemy dla nastepnej pary
+    }
+}
