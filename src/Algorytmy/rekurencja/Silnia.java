45bb745e1a4a4a1ed9ace9397a018ad5bacbd4b8
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-02-22 13:55:42 +0100

    Algorithms and data structures. Classwork.

--- src/Algorytmy/rekurencja/Silnia.java
+++ src/Algorytmy/rekurencja/Silnia.java
@@ -12,8 +12,13 @@ public class Silnia {
         System.out.println("Podaj n ");
         n = scanner.nextInt();
         System.out.println("Silnia od n = " + n + " wynosi " + silnia(n));
-        for (int i = 1; i < 100; i++) {
-            System.out.println("Silnia od n = " + i + " wynosi " + silnia(i));
+        //       for (int i = 1; i < 100; i++) {
+        //           System.out.println("Silnia od n = " + i + " wynosi " + silnia(i));
+        //         System.out.println("Podaj n ");
+        //         n = scanner.nextInt();
+        System.out.println("Suma dodawania 1+2+..+ " + n + " wynosi " + silniaDodawana(n));
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Suma dodawania od 1 do " + i + " wynosi " + silniaDodawana(i));
 
         }
     }
@@ -23,6 +28,15 @@ public class Silnia {
             return 1;
         } else
             return (n * silnia(n - 1));
+
+    }
+
+    public static int silniaDodawana(int n) {
+        if (n == 0) {
+            return 0;
+        } else
+            return (n + silniaDodawana(n - 1));
+
 
     }
 }
