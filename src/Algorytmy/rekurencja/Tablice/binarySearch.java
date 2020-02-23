146f81a2d0fc43e8750d9feb16131907e4ed714a
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-02-23 10:59:59 +0100

    Algorithms and data structures. Classwork. Arrays.

--- src/Algorytmy/rekurencja/Tablice/binarySearch.java
+++ src/Algorytmy/rekurencja/Tablice/binarySearch.java
@@ -1,18 +1,28 @@
 package Algorytmy.rekurencja.Tablice;
 
+import java.util.Random;
+
 public class binarySearch {
 
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int szukane = 7;
+        Random random = new Random();
+        szukane = random.nextInt(20);
+        System.out.println("Szukam "+ szukane);
         int b = isInArray(a, szukane);
-        System.out.println("Znaleziono " + b);
+        if (b != -1) {
+            System.out.println("Znaleziono, indeks to  " + b);
+        } else {
+            System.out.println("Nie znaleziono");
+        }
     }
 
     public static int isInArray(int[] a, int l) {
         /*
         param tablica a
         param szukanaliczba
+        zwraca indeks znalezionego elementu lub -1 gdy brak
          */
         int left = 0;
         int right = a.length - 1;
