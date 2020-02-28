7ebc71427acdcfb7677e11b72c0cf5ce71d858fd
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-02-28 17:02:49 +0100

    Addes modules for sorting.

---
+++ src/Algorytmy/rekurencja/Tablice/sort/BubbleSort.java
@@ -0,0 +1,21 @@
+package Algorytmy.rekurencja.Tablice.sort;
+
+public class BubbleSort {
+
+    public static void sort(int[] tab) {
+
+        for (int i = 0; i < tab.length - 1; i++) {
+
+            for (int j = 0; j < tab.length - i - 1; j++) {
+
+                if (tab[j] > tab[j + 1]) {
+
+                    // zamiana
+                    int temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ src/Algorytmy/rekurencja/Tablice/sort/InsertSort.java
@@ -0,0 +1,21 @@
+package Algorytmy.rekurencja.Tablice.sort;
+
+public class InsertSort {
+
+    public static void sort(int[] tab) {
+
+        for (int i = 1; i < tab.length; i++) {
+
+            int j = i;      // fragment [0, ..., i - 1] jest już posortowany
+            int temp = tab[j];
+
+            while (j > 0 && tab[j - 1] > temp) {
+
+                tab[j] = tab[j - 1];
+                j--;
+            }
+
+            tab[j] = temp;
+        }
+    }
+}
