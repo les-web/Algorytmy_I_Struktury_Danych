a9de7c0547e6311babf7a232e70812377057f9f9
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-02-29 12:19:09 +0100

    Classwork 20200229.

--- src/Algorytmy/rekurencja/Tablice/stack/StackTab.java
+++ src/Algorytmy/rekurencja/Tablice/stack/StackTab.java
@@ -3,21 +3,62 @@ package Algorytmy.rekurencja.Tablice.stack;
 public class StackTab {
 
     int[] stack;
-    int size;
-    
-    public int pop() {
-        return -1;
+    //    int size;
+    int index;
+
+    public StackTab(int size) {
+        //       this.size = size;
+        stack = new int[size];
     }
 
-    public int peek() {
-        return -1;
+    public int pop() throws Exception {
+        if (index == 0) {
+            throw new Exception("Stos jest pusty");
+        }
+        index = index - 1;
+        return stack[index];
+    }
+
+    public int peek() throws Exception {
+        if (index == 0) {
+            throw new Exception("Stos jest pusty");
+        }
+        return stack[index-1];
     }
 
     public int search(int value) {
-        return -1;
+        int out = -1;
+        for (int j = index-1; j >= 0; j--) {
+            if (stack[j] == value) {
+                out = j+1;
+                break;
+
+            }
+        }
+        return out;
     }
 
-    public int push(int value) {
-        return -1;
+    public int getSize() {
+        return index;
+    }
+
+    public int push(int value) throws Exception {
+        if (index >= stack.length - 1) {
+            throw new Exception("Nie można dodać więcej");
+        } else {
+            stack[index] = value;
+            index = index + 1;
+            return value;
+        }
+    }
+
+    public void print() {
+        System.out.println("[ ");
+        for (int i = 0; (i>0 && i < index-2); i++) {
+            System.out.print(stack[i] + " , ");
+
+        }
+        System.out.print(stack[index-1]+ "  ]");
+        System.out.println();
     }
 }
