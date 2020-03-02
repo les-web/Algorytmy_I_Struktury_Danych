9318ff04ae3fa1fc69d62344965014f52644237b
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-03-02 11:56:47 +0100

    Classwork 20200301.

---
+++ src/test20200302/Test20200302.java
@@ -0,0 +1,16 @@
+package test20200302;
+
+public class Test20200302 {
+
+    public static void main(String[] args) {
+        int liczba;
+        int a = 10;
+        System.out.println(a++ + a);
+        System.out.println(a);
+        int dystans = 12345;
+        double km;
+        km=dystans/1_000;
+        System.out.println(km);
+
+    }
+}
