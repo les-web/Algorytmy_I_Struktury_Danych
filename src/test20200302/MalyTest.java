31758b07b511af4605544363a61eef3f6ce1801c
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-07-04 08:54:37 +0200

    Classwork 20200301.

---
+++ src/test20200302/MalyTest.java
@@ -0,0 +1,23 @@
+package test20200302;
+
+public class MalyTest {
+
+    Integer i;
+    int j;
+
+
+
+    public static void main(String[] args) {
+
+        MalyTest t = new MalyTest();
+        t.go();
+    }
+
+    public void go() {
+   //     j=i;
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+
+}
